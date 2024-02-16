886665b147a52cfcefefc94d647db2d4c2f7b1d0
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2024-02-16 08:27:15 +0300

    Modernize

--- src/ua_parser.cpp
+++ src/ua_parser.cpp
@@ -4,37 +4,33 @@ using namespace ua_parser;
 
 uint8_t ua_parser::parse(std::string_view s)
 {
-	const auto begin = s.begin(), end = s.end();
-	auto it = begin;
-	while(it != end)
 	{
-		if(*it == '(')
-		{
-			++it;
-			break;
-		}
-		++it;
+		auto findParen = s.find('(');
+		if(findParen == s.npos)
+			return DEVICE_UNKNOWN;
+
+		s.remove_prefix(findParen + 1);
 	}
-	s = s.substr(it - begin);
-	if(s.size() < 3)return DEVICE_UNKNOWN; // Smallest match is 3 characters
-	else if(s.starts_with("Linux; Android"))return DEVICE_ANDROID | MOBILE;
+	if(s.size() < 3)return DEVICE_UNKNOWN; // Smallest match is 3 characters ("X11")
+	
+	if(s.starts_with("Linux; Android"))return DEVICE_ANDROID | MOBILE;
 	else if(s.starts_with("iP"))
 	{
-		s = s.substr(2);
+		s.remove_prefix(2);
 		if(s.starts_with("hone"))return DEVICE_IPHONE | MOBILE;
 		if(s.starts_with("ad"))return DEVICE_IPAD | MOBILE;
 		if(s.starts_with("od"))return DEVICE_IPOD | MOBILE;
 	}
 	else if(s.starts_with("Windows "))
 	{
-		s = s.substr(8);
+		s.remove_prefix(8);
 		if(s.starts_with("NT"))return DEVICE_WINDOWS;
 		if(s.starts_with("Phone"))return DEVICE_WINDOWS_PHONE | MOBILE;
 	}
 	else if(s.starts_with("Macintosh"))return DEVICE_MAC_OS;
 	else if(s.starts_with("X11"))
 	{
-		s = s.substr(3);
+		s.remove_prefix(3);
 		if(s.starts_with("; CrOS"))return DEVICE_CHROME_OS;
 		return DEVICE_LINUX;
 	}
